a0f91eb22c226be9212b3d4cd1fa03636e121cfe
LOrlandoAL <s21120218@alumnos.itsur.edu.mx>
2024-09-16 10:19:08 -0600

    Se realizo la practica de personalizacion de formulario de sitio  con django.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 
-from polls.models import Question, Choice
+from .models import Choice, Question
 
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date"]
+    list_filter = ["pub_date"]
+    
+
+
+admin.site.register(Question, QuestionAdmin)
